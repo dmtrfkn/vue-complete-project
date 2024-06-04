8b0050c0eee733b3e23063519141ff2551373da2
Дмитрий <fokin@MBP-Fokin.dlink>
2024-06-04 19:26:55 +0700

    Decompose App component, add subscribe on createPost event, change from v-bind + v-on on input to v-model

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <h4>Создание поста</h4>
-      <input
-        v-bind:value="title"
-        @input="title = $event.target.value"
-        class="input"
-        type="text"
-        placeholder="Название"
-      />
-      <input
-        v-bind:value="body"
-        @input="body = $event.target.value"
-        class="input"
-        type="text"
-        placeholder="Описание"
-      />
-      <button @click="createPost" class="btn">Создать пост</button>
-    </form>
-    <div class="post" v-for="post in posts">
-      <div><strong>Название:</strong> {{ post.title }}</div>
-      <div><strong>Описание:</strong> {{ post.body }}</div>
-    </div>
+    <PostForm @create="createPost" />
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
+import PostList from '@/components/PostList.vue';
+import PostForm from '@/components/PostForm.vue';
+
 export default {
+  components: {
+    PostForm,
+    PostList,
+  },
   data() {
     return {
       posts: [
@@ -34,20 +21,11 @@ export default {
         { id: 2, title: 'Javascript 2', body: 'Описание поста 2' },
         { id: 3, title: 'Javascript 3', body: 'Описание поста 3' },
       ],
-      title: '',
-      body: '',
     };
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      };
-      this.posts.push(newPost);
-      this.title = '';
-      this.body = '';
+    createPost(post) {
+      this.posts.push(post);
     },
   },
 };
@@ -64,33 +42,5 @@ export default {
 
 .app {
   padding: 20px;
-}
-
-.post {
-  padding: 15px;
-  margin-top: 15px;
-  border: 2px solid teal;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-}
-
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
-
-.btn {
-  align-self: flex-end;
-  margin-top: 15px;
-  padding: 10px 15px;
-  background: none;
-  border: 1px solid teal;
-  color: teal;
-  outline: none;
 }
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input v-model="post.title" class="input" type="text" placeholder="Название" />
+    <input v-model="post.body" class="input" type="text" placeholder="Описание" />
+    <button @click="createPost" class="btn">Создать пост</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: '',
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post);
+      this.post = {
+        title: '',
+        body: '',
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  /* width: 100%; */
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  border: 1px solid teal;
+  color: teal;
+  outline: none;
+}
+</style>
