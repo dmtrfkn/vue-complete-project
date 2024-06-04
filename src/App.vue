592242ee475611269012cc9a0905aa2ad42157fe
Дмитрий <fokin@MBP-Fokin.dlink>
2024-06-04 18:53:51 +0700

    Add funtional to button for creating posts

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="app">
-    <form>
+    <form @submit.prevent>
       <h4>Создание поста</h4>
-      <input class="input" type="text" placeholder="Название" />
-      <input class="input" type="text" placeholder="Описание" />
-      <button class="btn">Создать пост</button>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      />
+      <button @click="createPost" class="btn">Создать пост</button>
     </form>
     <div class="post" v-for="post in posts">
       <div><strong>Название:</strong> {{ post.title }}</div>
@@ -22,9 +34,22 @@ export default {
         { id: 2, title: 'Javascript 2', body: 'Описание поста 2' },
         { id: 3, title: 'Javascript 3', body: 'Описание поста 3' },
       ],
+      title: '',
+      body: '',
     };
   },
-  methods: {},
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+    },
+  },
 };
 </script>
 
