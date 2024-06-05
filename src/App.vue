8a4a8b670042c0af00697133958aaf18594c7875
Дмитрий <fokin@MBP-Fokin.dlink>
2024-06-05 10:38:54 +0700

    Used mounted lifecycle function

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <Button @click="showModal">Создать пост</Button>
-    <PostList @delete="deletePost" :posts="posts" />
+    <Button class="create_button" @click="showModal">Создать пост</Button>
+    <PostList v-if="!isPostsLoading" @delete="deletePost" :posts="posts" />
+    <div v-else>Идет загрузка...</div>
     <Modal v-model:show="modalVisible"><PostForm @create="createPost" /></Modal>
   </div>
 </template>
@@ -10,6 +11,7 @@
 <script>
 import PostList from '@/components/PostList.vue';
 import PostForm from '@/components/PostForm.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -18,12 +20,9 @@ export default {
   },
   data() {
     return {
-      posts: [
-        { id: 1, title: 'Javascript', body: 'Описание поста' },
-        { id: 2, title: 'Javascript 2', body: 'Описание поста 2' },
-        { id: 3, title: 'Javascript 3', body: 'Описание поста 3' },
-      ],
+      posts: [],
       modalVisible: false,
+      isPostsLoading: false,
     };
   },
   methods: {
@@ -37,6 +36,20 @@ export default {
     showModal() {
       this.modalVisible = true;
     },
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        this.posts = response.data;
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.isPostsLoading = false;
+      }
+    },
+  },
+  mounted() {
+    this.fetchPosts();
   },
 };
 </script>
@@ -52,5 +65,12 @@ export default {
 
 .app {
   padding: 20px;
+}
+.create_button {
+  margin: 15px 0;
+}
+.buttons {
+  display: flex;
+  gap: 15px;
 }
 </style>
